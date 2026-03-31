44757540ec5a1e8d9097b88a72d99f750c4a9f8c
Dim4ik-creator <litvinov06062006@gmail.com>
2026-03-31 18:57:06 +0300

    added settings

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 from fastapi import FastAPI
-from .routers import tasks  # Импортируем наш роутер задач
+
+
+import re
+import uuid
 
 app = FastAPI()
 
-fake_database =[{"id": 1, "title": "Задача 1"}, {"id": 2, "title": "Задача 2"}]
-
-app = FastAPI(
-    title="Мой супер To-Do API",
-    description="API для управления задачами, написанное по статье с Хабра",
-    version="1.0.0"
-)
-
-# Подключаем роутер к главному приложению
-app.include_router(tasks.router)
-
-@app.get("/")
-def root():
-    return {"message": "Добро пожаловать в API! Перейдите на /docs для документации."}
